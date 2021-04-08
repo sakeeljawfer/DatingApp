352fce7a0cd8baa52d0e0d621190fcfd70811ebc
sakeel jawfer <sakeeljawfer@gmail.com>
2021-04-08 13:26:12 +0530

    first section commit

---
+++ API/Data/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, Name)
+VALUES (1,"sakeel");
+INSERT INTO Users (Id, Name)
+VALUES (2,"akeel");
+INSERT INTO Users (Id, Name)
+VALUES (3,"ameer");
+INSERT INTO Users (Id, Name)
+VALUES (4,"sameer");
